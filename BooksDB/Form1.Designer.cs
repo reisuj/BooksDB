b9505996fe4e20e61a246efa92707e39731a280c
Brian Branch <reisuj@gmail.com>
2019-04-02 15:16:29 -0400

    Add project files.

---
+++ BooksDB/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BooksDB
+{
+    partial class frmTitles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtConn
+            // 
+            this.txtConn.Location = new System.Drawing.Point(218, 390);
+            this.txtConn.Name = "txtConn";
+            this.txtConn.Size = new System.Drawing.Size(362, 20);
+            this.txtConn.TabIndex = 0;
+            // 
+            // frmTitles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtConn);
+            this.Name = "frmTitles";
+            this.Text = "Titles Database";
+            this.Load += new System.EventHandler(this.frmTitles_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConn;
+    }
+}
+
---
+++ BooksDB/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+
+namespace BooksDB
+{
+    public partial class frmTitles : Form
+    {
+        OleDbConnection conn;
+        public frmTitles()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTitles_Load(object sender, EventArgs e)
+        {
+            var connString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=D:\repos\Databases\Books.accdb; Persist Security Info = False;";
+
+            conn = new OleDbConnection(connString);
+            conn.Open();
+            txtConn.Text = conn.ToString();
+        }
+    }
+}
